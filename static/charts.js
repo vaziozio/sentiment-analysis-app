1c89c88b8ea7a8bd3a184c6bcfb1f6cc36060172
dogrinea <douglasesmerio@bol.com.br>
2018-12-10 00:51:20 -0200

    Update charts.js

--- static/charts.js
+++ static/charts.js
@@ -65,12 +65,12 @@ $(function() {
     '07:00 AM', '08:00 AM',
     '09:00 AM', '10:00 AM',
     '11:00 AM', '12:00 PM',
-    '13:00 PM', '14:00 PM',
-    '15:00 PM', '16:00 PM',
-    '17:00 AM', '18:00 PM',
-    '19:00 PM', '20:00 PM',
-    '21:00 PM', '22:00 PM',
-    '23:00 PM']
+    '01:00 PM', '02:00 PM',
+    '03:00 PM', '04:00 PM',
+    '05:00 AM', '06:00 PM',
+    '07:00 PM', '08:00 PM',
+    '09:00 PM', '10:00 PM',
+    '11:00 PM']
     //chart instances
     var ctxNegative = document.getElementById("negativeChart").getContext('2d');
     var ctxNeutral = document.getElementById("neutralChart").getContext('2d');
